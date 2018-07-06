c7c99960699080b04230d9012602c804cada5a4d
Nils Hoffmann <nils.hoffmann@isas.de>
2018-07-06 12:46:57 +0200

    Updated optional handling.

--- io/src/main/java/uk/ac/ebi/pride/jmztab2/model/OptionColumn.java
+++ io/src/main/java/uk/ac/ebi/pride/jmztab2/model/OptionColumn.java
@@ -19,48 +19,64 @@ import de.isas.mztab2.io.serialization.Serializers;
 import de.isas.mztab2.model.IndexedElement;
 
 /**
- * Additional columns can be added to the end of the protein table. These column headers MUST start with the prefix "opt_".
- * Column names MUST only contain the following characters: 'A'-'Z', 'a'-'z', '0'-'9', '_', '-', '[', ']', and ':'.
+ * Additional columns can be added to the end of the protein table. These column
+ * headers MUST start with the prefix "opt_". Column names MUST only contain the
+ * following characters: 'A'-'Z', 'a'-'z', '0'-'9', '_', '-', '[', ']', and ':'.
  *
  * @author qingwei
  * @since 28/05/13
- * 
+ *
  */
 public class OptionColumn extends MZTabColumn {
-    /** Constant <code>OPT="opt"</code> */
+
+    /**
+     * Constant <code>OPT="opt"</code>
+     */
     public static final String OPT = "opt";
-    /** Constant <code>GLOBAL="global"</code> */
+    /**
+     * Constant <code>GLOBAL="global"</code>
+     */
     public static final String GLOBAL = "global";
 
     /**
-     * Get the optional column header, which start with the prefix "opt_".
-     * the format: opt_{IndexedElement[id]}_{value}. Spaces within the parameter's name MUST be replaced by '_'.
+     * Get the optional column header, which start with the prefix "opt_". the
+     * format: opt_{IndexedElement[id]}_{value}. Spaces within the parameter's
+     * name MUST be replaced by '_'.
      *
-     * @param element if the value relates to all replicates, we use "global" in header. Here, if user set element is null for
-     *                define for all replicates.
+     * @param element if the value relates to all replicates, we use "global" in
+     * header. Here, if user set element is null for define for all replicates.
      * @param value SHOULD NOT be empty.
      * @return a {@link java.lang.String} object.
      */
     public static String getHeader(IndexedElement element, String value) {
         if (MZTabStringUtils.isEmpty(value)) {
-            throw new IllegalArgumentException("Optional column's value should not be empty.");
+            throw new IllegalArgumentException(
+                "Optional column's value should not be empty.");
         }
 
-        return OPT + "_" + (element == null ? GLOBAL : Serializers.getElementName(element).get()) +
-                "_" + value.replaceAll(" ", "_");
+        return OPT + "_" + (element == null ? GLOBAL : Serializers.
+            getElementName(element).
+            orElseThrow(() ->
+            {
+                return new IllegalArgumentException(
+                    "Could not retrieve element name for " + element.toString());
+            }))
+            + "_" + value.replaceAll(" ", "_");
     }
 
     /**
-     * Create a optional column. Which header start with the prefix "opt_", logical position always stay the end of table.
+     * Create a optional column. Which header start with the prefix "opt_",
+     * logical position always stay the end of table.
      *
      * @see #getHeader() generate optional column header.
-     * @param element if the value relates to all replicates, we use "global" in header. Here, if user set element is null for
-     *                define for all replicates.
+     * @param element if the value relates to all replicates, we use "global" in
+     * header. Here, if user set element is null for define for all replicates.
      * @param value SHOULD NOT be empty.
      * @param columnType SHOULD NOT be empty.
      * @param offset SHOULD be positive integer.
      */
-    public OptionColumn(IndexedElement element, String value, Class columnType, int offset) {
+    public OptionColumn(IndexedElement element, String value, Class columnType,
+        int offset) {
         super(getHeader(element, value), columnType, true, offset + 1 + "");
     }
 }
